6255fa43a7b2a4fb377530354ec6032ace3cde0a
Stefan Bürk <stefan@buerk.tech>
2026-04-06 20:01:25 +0200

    [TASK] DPL-158: Cleanup unused stuff
    Removes unused stuff like tailor package exclude
    definition and adds PHP8.5 for TYPO3 v13 testing.

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -286,6 +286,7 @@ Options:
             - 8.2: use PHP 8.2
             - 8.3: use PHP 8.3
             - 8.4: use PHP 8.4
+            - 8.5: use PHP 8.5
 
     -x
         Only with -s functional|unit|unitRandom
@@ -393,7 +394,7 @@ while getopts "a:b:s:d:i:p:t:xy:o:nhu" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3|8.4)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3|8.4|8.5)$ ]]; then
                 INVALID_OPTIONS+=("p ${OPTARG}")
             fi
             ;;
